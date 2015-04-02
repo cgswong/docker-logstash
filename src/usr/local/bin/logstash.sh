9a6e44c74422d9afb3a5377065c058739b444984
cgwong <cgwong@cgwong-mac.na.ds.monsanto.com>
2015-04-02 15:10:02 -0500

    Re-add REDIS var to cfg, refactor script code.

--- src/usr/local/bin/logstash.sh
+++ src/usr/local/bin/logstash.sh
@@ -18,47 +18,52 @@
 # 2015/03/25 cgwong v1.0.0: Refactor.
 # #################################################################
 
-# Fail hard and fast
+# Fail fast, including pipelines
 set -eo pipefail
 
-# Set environment variables
+# Logstash setup
 LS_HOME="/opt/logstash"
 LS_CFG_FILE=${LS_CFG_FILE:-"/etc/logstash/conf.d/logstash.conf"}
 
-LSF_CERT_FILE=${LSF_CERT_FILE:-"/etc/logstash/ssl/logstash-forwarder.cert"}
+# Logstash-Forwarder setup
+LSF_CERT_FILE=${LSF_CERT_FILE:-"/etc/logstash/ssl/logstash-forwarder.crt"}
 LSF_KEY_FILE=${LSF_KEY_FILE:-"/etc/logstash/ssl/logstash-forwarder.key"}
 
+# Elasticsearch setup
 ES_CLUSTER=${ES_CLUSTER:-"es01"}
+
+# Use linked ES container if exist, otherwise ES_HOST if defined, else fall back to localhost
 ES_HOST=${ES_HOST:-"localhost"}
+ES_HOST=${ES_PORT_9200_TCP_ADDR:-$ES_HOST}
+
+# Use linked ES container if exist, otherwise ES_PORT if defined, else fall back to 9200
 ES_PORT=${ES_PORT:-"9200"}
+ES_PORT=${ES_PORT_9200_TCP_PORT:-$ES_PORT}
 
 REDIS_HOST=${REDIS_HOST:-"localhost"}
 REDIS_PORT=${REDIS_PORT:-"6379"}
 
 createKey() {
-  # Create a new SSL certificate for Logstash-Forwarder if needed
+# Create a new SSL certificate for Logstash-Forwarder if needed
   if [ ! -f "$LSF_KEY_FILE" ]; then
     echo "[logstash] Generating new logstash-forwarder key"
     openssl req -x509 -batch -nodes -newkey rsa:4096 -keyout "$LSF_KEY_FILE" -out "$LSF_CERT_FILE"
   fi
 }
 
-# Download the config file, if given a URL
-if [ ! -z "$LS_CFG_URL" ]; then
-  echo "[logstash] Downloading logstash config file from ${LS_CFG_URL}"
-  curl -Ls -o ${LS_CFG_FILE} ${LS_CFG_URL}
-  if [ $? -ne 0 ]; then
-    echo "[logstash] Failed to download ${LS_CFG_URL} exiting."
-    exit 1
+downloadLogstashConfig() {
+  # Download the config file, if given a URL
+  if [ ! -z "$LS_CFG_URL" ]; then
+    echo "[logstash] Downloading logstash config file from ${LS_CFG_URL}"
+    curl -Ls -o ${LS_CFG_FILE} ${LS_CFG_URL}
+    if [ $? -ne 0 ]; then
+      echo "[logstash] Failed to download ${LS_CFG_URL} exiting."
+      exit 1
+    fi
   fi
+}
 
-  # Update logstash.conf file with needed info
-  sed -ie "s/ES_CLUSTER/${ES_CLUSTER}/" $LS_CFG_FILE
-  sed -ie "s/ES_HOST/${ES_HOST}/" $LS_CFG_FILE
-  sed -ie "s/ES_PORT/${ES_PORT}/" $LS_CFG_FILE
-  sed -ie "s/REDIS_HOST/${REDIS_HOST}/" $LS_CFG_FILE
-  sed -ie "s/REDIS_PORT/${REDIS_PORT}/" $LS_CFG_FILE
-
+downloadLSFConfig() {
   # Download SSL cert if given a URL
   if [ ! -z "$LSF_CERT_URL" ]; then
     echo "[logstash] Downloading logstash-forwarder cert from ${LSF_CERT_URL}"
@@ -77,57 +82,81 @@ if [ ! -z "$LS_CFG_URL" ]; then
       exit 1
     fi
   fi
+}
 
-  createKey
+SanitizeLogstashConfig() {
+  # Update logstash.conf file with needed info if they exist
+  sed -e "s|ES_CLUSTER|${ES_CLUSTER}|g" \
+      -e "s|ES_HOST|${ES_HOST}|g" \
+      -e "s|ES_PORT|${ES_PORT}|g" \
+      -e "s|REDIS_HOST|${REDIS_HOST}|g" \
+      -e "s|REDIS_PORT|${REDIS_PORT}|g" \
+      -i "$LS_CFG_FILE"
+}
 
+validateLogstashConfig() {
   # Check/validate config file
   ${LS_HOME}/bin/logstash agent -t -f ${LS_CFG_FILE}
   if [ $? -ne 0 ]; then
-    echo "[logstash] Invalid logstash.conf file, exiting."
+    echo "[logstash] Invalid ${LS_CFG_FILE} file, exiting."
     exit 1
   fi
-elif [ -z "$LS_CFG_USE" ]; then
-  # Use service discovery
-  KV_TYPE=${KV_TYPE:-etcd}
-  KV_HOST=${KV_HOST:-127.0.0.1}
-  if [ "$KV_TYPE" = "etcd" ]; then
-    # Set as default for etcd unless otherwise stated
-    KV_PORT=${KV_PORT:-4001}
-  else
-    # Set as default for consul unless otherwise stated
-    KV_PORT=${KV_PORT:-8500}
+}
+
+configKV() {
+# Use service discovery
+
+  SVC_URL_BASE="services/logging/logstash"
+  LS_CONFD_CFG="/etc/confd/conf.d/logstash.toml"
+  LS_CONFD_TMPL="/etc/confd/templates/logstash.conf.tmpl"
+
+  if [ ! -z "$KV_TYPE" ]; then
+    KV_TYPE=${KV_TYPE:-etcd}
+    KV_HOST=${KV_HOST:-127.0.0.1}
+    if [ "$KV_TYPE" = "etcd" ]; then
+      # Set as default for etcd unless otherwise stated
+      KV_PORT=${KV_PORT:-4001}
+      KV_URL=${KV_HOST}:${KV_PORT}
+
+      # Publish SSL cert/key to KV store
+      curl -L $KV_URL/v2/keys/${SVC_URL_BASE}/ssl_certificate -XPUT --data-urlencode value@${LSF_CERT_FILE}
+      curl -L $KV_URL/v2/keys/${SVC_URL_BASE}/ssl_private_key -XPUT --data-urlencode value@${LSF_KEY_FILE}
+    elif [ "$KV_TYPE" = "consul" ]; then
+      # Set as default for consul unless otherwise stated
+      KV_PORT=${KV_PORT:-8500}
+      KV_URL=${KV_HOST}:${KV_PORT}
+
+      # Publish SSL cert/key to KV store
+      curl -L $KV_URL/v1/kv/${SVC_URL_BASE}/ssl_certificate -XPUT --data-urlencode @${LSF_CERT_FILE}
+      curl -L $KV_URL/v1/kv/${SVC_URL_BASE}/ssl_private_key -XPUT --data-urlencode @${LSF_KEY_FILE}
+    else
+      echo "[logstash] Invalid KV_TYPE specified, valid values are etcd and consul."
+      exit 1
+    fi
+
+    # Update ES_CLUSTER resources
+    sed -ie "s|es01|${ES_CLUSTER}|g" ${LS_CONFD_CFG}
+    sed -ie "s|es01|${ES_CLUSTER}|g" ${LS_CONFD_TMPL}
+
+    echo "[logstash] booting container using $KV_TYPE KV backend"
+
+    # Loop every 5 seconds until confd has updated the logstash config
+    until confd -onetime -backend $KV_TYPE -node $KV_URL -config-file $LS_CONFD_CFG; do
+      echo "[logstash] waiting for confd to refresh logstash.conf (waiting for ElasticSearch to be available)"
+      sleep 5
+    done
+
+    # Put continual polling on `confd` process in the background to watch for any changes every 10 seconds.
+    confd -interval 10 -backend $KV_TYPE -node $KV_URL -config-file $LS_CONFD_CFG &
+    echo "[logstash] confd is now monitoring $KV_TYPE for any changes..."
   fi
-  KV_URL=${KV_HOST}:${KV_PORT}
+}
 
-  # Update ES_CLUSTER resources
-  sed -ie "s/es01/$ES_CLUSTER/g" /etc/confd/conf.d/logstash.toml
-  sed -ie "s/es01/$ES_CLUSTER/g" /etc/confd/templates/logstash.conf.tmpl
-
-  echo "[logstash] booting container using $KV_TYPE KV backend"
-
-  # Loop every 5 seconds until confd has updated the logstash config
-  until confd -onetime -backend $KV_TYPE -node $KV_URL -config-file /etc/confd/conf.d/logstash.toml; do
-    echo "[logstash] waiting for confd to refresh logstash.conf (waiting for ElasticSearch to be available)"
-    sleep 5
-  done
-
-  # Put continual polling on `confd` process in the background to watch for any changes every 10 seconds.
-  confd -interval 10 -backend $KV_TYPE -node $KV_URL -config-file /etc/confd/conf.d/logstash.toml &
-  echo "[logstash] confd is now monitoring $KV_TYPE for any changes..."
-
-  createKey
-
-  # Publish SSL cert/key to KV store
-  if [ "$KV_TYPE" == "etcd" ]; then
-    # Etcd as KV store
-    curl -L $KV_URL/v2/keys/services/logging/logstash/ssl_certificate -XPUT --data-urlencode value@${LS_SSL}/logstash-forwarder.crt
-    curl -L $KV_URL/v2/keys/services/logging/logstash/ssl_private_key -XPUT --data-urlencode value@${LS_SSL}/logstash-forwarder.key
-  else
-    # Assume it's consul KV otherwise
-    curl -L $KV_URL/v1/kv/services/logging/logstash/ssl_certificate -XPUT --data-urlencode @${LS_SSL}/logstash-forwarder.crt
-    curl -L $KV_URL/v1/kv/services/logging/logstash/ssl_private_key -XPUT --data-urlencode @${LS_SSL}/logstash-forwarder.key
-  fi
-fi
+downloadLogstashConfig
+SanitizeLogstashConfig
+downloadLSFConfig
+createKey
+configKV
 
 # Run process, if `docker run` first argument start with `--` the user is passing launcher arguments
 if [ $# -lt 1 ]; then
